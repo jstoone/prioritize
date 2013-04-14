90dfc08957be7cff99caa00b0cb28217c7ad9961
jstoone <jakob@meat-software.org>
2013-04-14 15:47:07 +0200

    Added basic markup and edit functionality.

--- application/views/prioritize.php
+++ application/views/prioritize.php
@@ -1,25 +1,27 @@
 <div class="gridster row-fluid">
-        <ul class="span12">
-            <li class="span2" data-row="1" data-col="1" data-sizex="1" data-sizey="1"></li>
-            <li class="span2" data-row="1" data-col="2" data-sizex="1" data-sizey="1"></li>
-            <li class="span2" data-row="1" data-col="3" data-sizex="1" data-sizey="1"></li>
-            <li class="span2" data-row="1" data-col="4" data-sizex="1" data-sizey="1"></li>
-            <li class="span2" data-row="1" data-col="5" data-sizex="1" data-sizey="1"></li>
-            <li class="span2" data-row="1" data-col="6" data-sizex="1" data-sizey="1"></li>
+    <div class="stickies span12">
+            <div class="span2" data-row="1" data-col="1" data-sizex="1" data-sizey="1">
+                New note text.
+            </div>
+            <div class="span2" data-row="1" data-col="2" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="1" data-col="3" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="1" data-col="4" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="1" data-col="5" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="1" data-col="6" data-sizex="1" data-sizey="1"></div>
 
-            <li class="span2" data-row="2" data-col="1" data-sizex="1" data-sizey="1"></li>
-            <li class="span2" data-row="2" data-col="2" data-sizex="1" data-sizey="1"></li>
-            <li data-row="2" data-col="3" data-sizex="1" data-sizey="1"></li>
-            <li data-row="2" data-col="4" data-sizex="1" data-sizey="1"></li>
-            <li data-row="2" data-col="5" data-sizex="1" data-sizey="1"></li>
-            <li data-row="2" data-col="6" data-sizex="1" data-sizey="1"></li>
+            <div class="span2" data-row="2" data-col="1" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="2" data-col="2" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="2" data-col="3" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="2" data-col="4" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="2" data-col="5" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="2" data-col="6" data-sizex="1" data-sizey="1"></div>
 
-            <li data-row="3" data-col="1" data-sizex="1" data-sizey="1"></li>
-            <li data-row="3" data-col="2" data-sizex="1" data-sizey="1"></li>
-            <li data-row="3" data-col="3" data-sizex="1" data-sizey="1"></li>
-            <li data-row="3" data-col="4" data-sizex="1" data-sizey="1"></li>
-            <li data-row="3" data-col="5" data-sizex="1" data-sizey="1"></li>
-            <li data-row="3" data-col="6" data-sizex="1" data-sizey="1"></li>
-        </ul>
+            <div class="span2" data-row="3" data-col="1" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="3" data-col="2" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="3" data-col="3" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="3" data-col="4" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="3" data-col="5" data-sizex="1" data-sizey="1"></div>
+            <div class="span2" data-row="3" data-col="6" data-sizex="1" data-sizey="1"></div>
     </div>
+</div>
 </di>
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,38 @@
-$(document).ready(function(){
-    $(".gridster ul").gridster({
+function setupGridster(){
+    $(".gridster div.stickies").gridster({
+        widget_selector: "div.stickies div",
         widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140]
+        widget_base_dimensions: [201, 201]
     });
+}
+
+
+
+function setupStickyEdit() {
+    function exitEdit(){
+        var textarea = $(this);
+        var text = textarea.val();
+        var p = $('<p>').text(text);
+        textarea.after(p);
+        textarea.remove();
+
+    }
+
+
+    $('.gridster div.stickies div').dblclick(function(e){
+        var inputField = $('<textarea id="note">');
+        var text = $(this).text().trim();
+        inputField.text(text);
+        $(this).html(inputField);
+        inputField.focus();
+
+        inputField.focusout(exitEdit);
+    });
+}
+
+
+$(document).ready(function(){
+    setupGridster();
+
+    setupStickyEdit();
 });
