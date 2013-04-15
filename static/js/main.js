f60a8be25aa943f7afc7a95f86a6a89a65db482c
jstoone <jakob@meat-software.org>
2013-04-15 10:07:04 +0200

    Reformatted code, and minor tweaks in js and css

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,32 @@
-function setupGridster(){
+function setupGridster() {
     $(".gridster div.stickies").gridster({
         widget_selector: "div.stickies div",
         widget_margins: [10, 10],
         widget_base_dimensions: [137, 137],
-        min_cols: 6
+        min_cols: 6,
+        extra_rows: 6
     });
+
 }
 
-function setupStickyAdd(){
-    function addSticky(color){
+function setupStickyAdd() {
+    function addSticky(color) {
         var gridster = $('.gridster div.stickies').gridster().data('gridster');
-        var randomCol = Math.floor(Math.random()*2)+3;
+        var randomCol = Math.floor(Math.random() * 2) + 3;
         var sticky = $('<div>');
-        if(color.toString() == "pink"){
+        if (color.toString() == "pink") {
             sticky.addClass('pink');
             gridster.add_widget(sticky, 1, 1, 1, 1);
         }
-        if(color.toString() == "orange"){
+        if (color.toString() == "orange") {
             sticky.addClass('orange');
             gridster.add_widget(sticky, 1, 1, 2, 1);
         }
-        if(color.toString() == "yellow"){
+        if (color.toString() == "yellow") {
             sticky.addClass('yellow');
             gridster.add_widget(sticky, 1, 1, randomCol, 1);
         }
-        if(color.toString() == "green"){
+        if (color.toString() == "green") {
             sticky.addClass('green');
             gridster.add_widget(sticky, 1, 1, randomCol + 2, 1);
         }
@@ -32,14 +34,14 @@ function setupStickyAdd(){
 
     }
 
-    $('header ul.nav li a').click(function(){
+    $('header ul.nav li a').click(function () {
         var color = $(this).attr('class');
         addSticky(color);
     });
 }
 
 function setupStickyEdit() {
-    function exitEdit(){
+    function exitEdit() {
         var textarea = $(this);
         var text = textarea.val();
         var p = $('<p>').text(text);
@@ -48,8 +50,7 @@ function setupStickyEdit() {
     }
 
 
-    $(document).on('dblclick', 'div.gs_w', function(){
-        console.log('blob');
+    $(document).on('dblclick', 'div.gs_w', function () {
         var inputField = $('<textarea id="note">');
         var text = $(this).text().trim();
         inputField.text(text);
@@ -61,7 +62,7 @@ function setupStickyEdit() {
 }
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     setupGridster();
     setupStickyAdd();
     setupStickyEdit();
